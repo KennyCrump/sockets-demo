5a47081d845512ad43184fdb31a5ab9e3b57ea4b
Kenny Crump <kennycrump@Kennys-MacBook-Pro.local>
2019-01-18 10:51:55 -0700

    added dynamic rooms and general chatroom

--- src/Components/Room.js
+++ src/Components/Room.js
@@ -5,7 +5,7 @@ class Room extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      messages: ["hello", "hey"],
+      messages: [],
       message: "",
       userTyping: false
     };
